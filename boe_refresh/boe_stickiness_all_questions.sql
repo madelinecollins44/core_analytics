cef403810fa3c16cc6b17c28297cc5959348c1a7
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-01 17:47:03 -0400

    Update boe_stickiness_all_questions.sql

--- boe_refresh/boe_stickiness_all_questions.sql
+++ boe_refresh/boe_stickiness_all_questions.sql
@@ -47,3 +47,20 @@ group by all
 select sum(join_before_download) as join_before_download, sum(join_with_download) as join_with_download, sum(join_after_download) as join_after_download, sum(no_account) as no_account from agg 
 
 --how did users search in their first visit
+create or replace table etsy-data-warehouse-dev.madelinecollins.app_downloads_had_search_first_visit as (
+select 
+  a.user_id
+  , a.browser_id
+  , a.download_date
+  , b._date as visit_date 
+  , b.visit_id
+from etsy-data-warehouse-dev.semanuele.boe_stickiness_all a 
+inner join etsy-data-warehouse-prod.weblog.visits b
+  on (a.user_id=b.user_id or a.user_id is null and b.user_id is null)
+  and a.browser_id=b.browser_id
+  and a.download_date=b._date
+where 
+  a.had_search_first_visit = 1
+  and b._date >= "2022-01-01"
+  and b.platform in ('boe')
+); 
