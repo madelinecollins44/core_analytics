c7f1612f7b0e3ee57f65ca230599cf98ab32dfbd
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-01 17:03:39 -0400

    Update boe_stickiness_all_questions.sql

--- boe_refresh/boe_stickiness_all_questions.sql
+++ boe_refresh/boe_stickiness_all_questions.sql
@@ -25,6 +25,7 @@ from agg
 --SELECT 9892131/36258055 = 0.27 
 
 --different joining moments 
+--how many buyers (user_id + browser_id combo) created an account prior to download_date
 with agg as (
 select
   a.user_id
@@ -34,6 +35,7 @@ select
   , case when date(timestamp_seconds(b.join_date)) < a.download_date then 1 else 0 end as join_before_download
   , case when date(timestamp_seconds(b.join_date)) = a.download_date then 1 else 0 end as join_with_download
   , case when date(timestamp_seconds(b.join_date)) > a.download_date then 1 else 0 end as join_after_download
+  , case when date(timestamp_seconds(b.join_date)) is null then 1 else 0 end as no_account
 from 
   etsy-data-warehouse-dev.semanuele.boe_stickiness_all a
 left join 
@@ -42,4 +44,4 @@ left join
 -- where date(timestamp_seconds(b.join_date)) >= a.download_date
 group by all 
 )
-select sum(join_before_download), sum(join_with_download), sum(join_after_download) from agg 
+select sum(join_before_download) as join_before_download, sum(join_with_download) as join_with_download, sum(join_after_download) as join_after_download, sum(no_account) as no_account from agg 
