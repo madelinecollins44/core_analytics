6529b453806e1fdac182591acdd53bf6ab368703
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-01 16:53:41 -0400

    Update boe_stickiness_all_questions.sql

--- boe_refresh/boe_stickiness_all_questions.sql
+++ boe_refresh/boe_stickiness_all_questions.sql
@@ -1,1 +1,45 @@
+-----HOW MANY USER, BROWSER COMBOS ARE THERE? 
+select count(*) from etsy-data-warehouse-dev.semanuele.boe_stickiness_all 
+--36258055
 
+--how many buyers (user_id + browser_id combo) created an account prior to download_date
+with agg as (
+select
+  a.user_id
+  , a.browser_id
+  -- , concat(a.user_id,'|' ,a.browser_id) as concat
+  , a.download_date
+  , date(timestamp_seconds(b.join_date)) as recipient_join_date
+from 
+  etsy-data-warehouse-dev.semanuele.boe_stickiness_all a
+left join 
+  etsy-data-warehouse-prod.user_mart.mapped_user_profile b 
+    on a.user_id = b.mapped_user_id
+where date(timestamp_seconds(b.join_date))  < a.download_date
+)
+select 
+count(*)
+from agg 
+--9892131
+
+--SELECT 9892131/36258055 = 0.27 
+
+--different joining moments 
+with agg as (
+select
+  a.user_id
+  , a.browser_id
+  , a.download_date
+  , date(timestamp_seconds(b.join_date)) as recipient_join_date
+  , case when date(timestamp_seconds(b.join_date)) < a.download_date then 1 else 0 end as join_before_download
+  , case when date(timestamp_seconds(b.join_date)) = a.download_date then 1 else 0 end as join_with_download
+  , case when date(timestamp_seconds(b.join_date)) > a.download_date then 1 else 0 end as join_after_download
+from 
+  etsy-data-warehouse-dev.semanuele.boe_stickiness_all a
+left join 
+  etsy-data-warehouse-prod.user_mart.mapped_user_profile b 
+    on a.user_id = b.mapped_user_id
+-- where date(timestamp_seconds(b.join_date)) >= a.download_date
+group by all 
+)
+select sum(join_before_download), sum(join_with_download), sum(join_after_download) from agg 
