a3578414701495eb3cc045a2bad67ff6ebeb33fe
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-02 09:23:42 -0400

    Update boe_stickiness_all_questions.sql

--- boe_refresh/boe_stickiness_all_questions.sql
+++ boe_refresh/boe_stickiness_all_questions.sql
@@ -47,12 +47,13 @@ group by all
 select sum(join_before_download) as join_before_download, sum(join_with_download) as join_with_download, sum(join_after_download) as join_after_download, sum(no_account) as no_account from agg 
 
 ---------HOW DID USERS SEARCH IN THEIR FIRST VISIT
---pull first visit id here
+--base table 1: first visit
 create or replace table etsy-data-warehouse-dev.madelinecollins.app_downloads_had_search_first_visit as ( -- stole this logic from sam 
  select
     b.browser_id,
     b.user_id,
     b.download_date,
+    a.visit_id,
     a.query_raw,
     a.query,
     a.has_click,
@@ -72,7 +73,7 @@ create or replace table etsy-data-warehouse-dev.madelinecollins.app_downloads_ha
     and a.platform in ('boe')
   group by all
 );
- 
+
 
   --pull all data associated w queries in visits on first day 
 create or replace table etsy-data-warehouse-dev.madelinecollins.app_downloads_had_search_first_visit as ( -- stole this logic from sam 
@@ -96,49 +97,8 @@ create or replace table etsy-data-warehouse-dev.madelinecollins.app_downloads_ha
   where a._date >= "2022-01-01" and a._date <= "2023-06-01"
 );
 
---get engagmenet around query 
-create or replace table etsy-data-warehouse-dev.madelinecollins.boe_stickiness_had_search_first_visit_queries as (
-select 
-  a.user_id
-  , a.browser_id
-  , a.visit_id
-  , b.query
-  , c.bin
-  , b.has_click
-  , b.has_favorite
-  , b.has_cart	
-  , b.has_purchase
-from 
-  etsy-data-warehouse-dev.madelinecollins.app_downloads_had_search_first_visit a
-left join 
-  etsy-data-warehouse-prod.search.query_sessions_new b
-    using (visit_id)
-left join etsy-data-warehouse-prod.search.query_bins c
-  on b.query_raw=c.query_raw
-where 
-  b.platform in ('boe')
-  and b._date >= '2022-01-01'
-);
-
-
---most common search queries in first day visits 
-with words as (
-select 
-  word
- , search_visit_id as visit_id
- , user_id
- , browser_id
-from etsy-data-warehouse-dev.madelinecollins.app_downloads_had_search_first_visit,
-unnest(split(query, ' ')) as word
-)
-select
-  word
-  , count(visit_id) as searches 
-  , count(distinct user_id) as users
-  , count(distinct browser_id) as browsers
-from words
-group by all order by 2 desc 
-
+--engagements around query 
+--switch out base tables to get different levels of interaction 
 
 
 
