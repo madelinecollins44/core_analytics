584452a4d52e24fe5fce3387eb1f1905f9fc058a
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-29 13:12:46 -0400

    Update boe_churn.sql

--- boe_health/boe_churn.sql
+++ boe_health/boe_churn.sql
@@ -10,58 +10,35 @@ select
   , visit_id
   , _date
   , start_datetime
-  , unix_seconds(timestamp (start_datetime)) as start_time)
+  , unix_seconds(timestamp (start_datetime)) as start_time
   , row_number() over (partition by user_id order by unix_seconds(timestamp (start_datetime)) desc, visit_id desc) AS visit_order
 from 
   etsy-data-warehouse-prod.weblog.visits
 where _date >= current_date-10
 )
-select * from agg_visit where user_id=266926560
-
-
-, last_boe_visit as (
+, last_visit_platform as (
 select 
-    user_id
-    , max(timestamp(split(visit_id, ".")[offset(1)])) as most_recent_visit
-  -- , max(start_time) as most_recent_visit
-from 
- (select user_id, max(start_time) from agg_visit_data where platform in ('boe')) a -- this gives me most recent day
-group by all
+  user_id
+  , platform
+  , min(visit_order) as most_recent_visit
+from agg_visit
+where user_id=266926560
+group by all 
 )
-select user_id, count(*) from last_boe_visit group by all order by 2 desc
-
-
--- , last_boe as (
--- select a.* 
--- from agg_visit_data a
--- inner join last_boe_visit b
---   on a.user_id=b.user_id
---   and a.start_time=b. most_recent_visit
--- )
--- select * from last_boe where user_id = 266926560
--- -- select user_id, count(*) from last_boe group by all order by 2 desc
-
-
-
--- -- , last_visit as (
--- -- select
--- --   user_id
--- --   , max(start_time) as most_recent_visit
--- -- from 
--- --   agg_visit_data
--- -- group by all
--- -- )
--- -- select
--- --   user_id
--- --   , platform 
--- --   , browser_platform
--- -- 	, region  
--- --   , admin
--- --   , top_channel 
--- -- from 
--- --   agg_visit_data
--- -- left join 
--- --   last_boe_visit
--- --     using ()
--- -- left join 
--- --   last_visit
+select
+  a.user_id
+  , a.platform 
+  , a.browser_platform
+	, a.region  
+  , a.admin
+  , a.top_channel 
+  , case when b.platform in ('boe') then (current_date- a._date) end as days_since_boe_visit
+  -- days_since_mweb_visit
+  -- days_since_detskop_visit
+from 
+  agg_visit a 
+left join 
+  last_visit_platform b
+    on a.user_id=b.user_id
+    and a.platform=b.platform
+    and a.visit_order=b.most_recent_visit
