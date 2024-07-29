afd40ec83524a908b57235b9ded58c68e0206dc3
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-29 17:04:38 -0400

    Update new_user_retention.sql

--- boe_health/new_user_retention.sql
+++ boe_health/new_user_retention.sql
@@ -18,6 +18,8 @@ qualify row_number() over(partition by browser_id order by start_datetime) = 1
 )
 select 
 is_signed_in,
+browser_platform,
+region,
 -- agg totals
 count(distinct case when first_app_visit = next_visit_date then browser_id end) as next_day_visits,
 count(distinct case when next_visit_date <= first_app_visit + 6 then browser_id end) as first_7_days,
@@ -28,6 +30,6 @@ count(distinct case when first_app_visit = next_visit_date then browser_id end)/
 count(distinct case when next_visit_date <= first_app_visit + 6 then browser_id end)/count(distinct browser_id) as pct_first_7_days,
 count(distinct case when next_visit_date <= first_app_visit + 13 then browser_id end)/count(distinct browser_id) as pct_first_14_days,
 count(distinct case when next_visit_date <= first_app_visit + 29 then browser_id end)/count(distinct browser_id) as pct_first_30_days
-from `etsy-data-warehouse-dev.semanuele.first_visits`
+from first_visits
 where first_app_visit <= current_date
 group by all
