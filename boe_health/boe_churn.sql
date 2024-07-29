670224b4bd9306fc34a32ff2064bec5f6c0ad2d4
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-29 13:59:29 -0400

    Update boe_churn.sql

--- boe_health/boe_churn.sql
+++ boe_health/boe_churn.sql
@@ -3,9 +3,9 @@ BEGIN
 
 declare last_date date;
 
--- drop table if exists `etsy-data-warehouse-prod.rollups.boe_churn_segmentation`;
+drop table if exists `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`;
 
--- create table if not exists `etsy-data-warehouse-prod.rollups.boe_churn_segmentation`  (
+-- create table if not exists `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`  (
 create table if not exists `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`  (
  _date DATE
   , region STRING
@@ -30,28 +30,28 @@ select
  a.user_id
   , a.platform 
   , a.browser_platform
-	, a.region  
-  , a.is_admin_visit as admin
-  , a.top_channel 
+	-- , a.region  
+  -- , a.is_admin_visit as admin
+  -- , a.top_channel 
   , a.visit_id
   , a._date
   , a.start_datetime
   , unix_seconds(timestamp (a.start_datetime)) as start_time
   , row_number() over (partition by a.user_id order by unix_seconds(timestamp (a.start_datetime)) desc, a.visit_id desc) AS visit_order
-  , b.buyer_segment
+  -- , b.buyer_segment
 from 
   etsy-data-warehouse-prod.weblog.visits a 
 left join 
   etsy-data-warehouse-prod.rollups.visits_w_segments b using (visit_id, user_id, _date, platform)
-where _date >= last_date
+where _date >= current_date-3
 )
 , last_visit_platform as (
 select 
   user_id
-  , region
-  , admin
+  -- , region
+  -- , admin
   , platform
-  , buyer_segment
+  -- , buyer_segment
   , min(visit_order) as most_recent_visit
   , cast(max(_date) as date) as most_recent_visit_date
 from agg_visit
@@ -59,11 +59,10 @@ group by all
 )
 select
     user_id
-  , region
-  , admin
-  , buyer_segment
+  -- , region
+  -- , admin
+  -- , buyer_segment
   , current_date as _date
-  , sum(total_visits) as total_visits
   , coalesce(max(case when platform in ('boe') then date_diff(current_date, most_recent_visit_date, day) else null end),0) as days_since_boe_visit
   , coalesce(max(case when platform in ('mobile_web') then date_diff(current_date, most_recent_visit_date, day) else null end),0) as days_since_mweb_visit
   , coalesce(max(case when platform in ('desktop') then date_diff(current_date, most_recent_visit_date, day)else null end),0) as days_since_desktop_visit
@@ -75,9 +74,9 @@ group by all
 insert into `etsy-data-warehouse-dev.rollups.boe_churn_segmentation` (
 select
   _date
-  , region
-  , admin
-  , buyer_segment
+  -- , region
+  -- , admin
+  -- , buyer_segment
   , days_since_boe_visit
   , days_since_mweb_visit
   , days_since_desktop_visit
