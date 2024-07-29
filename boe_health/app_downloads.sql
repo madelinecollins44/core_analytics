7ebea9be411abe8c13b72fae64431503042f9bb5
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-29 14:27:34 -0400

    Create app_downloads.sql

---
+++ boe_health/app_downloads.sql
@@ -0,0 +1,33 @@
+with all_downloads as (
+--organic installs 
+select
+  Country_Code
+  , date(Event_Time) as download_date
+  , platform
+  , advertising_id 
+  , 'organic' as download_type
+from
+  etsy-data-warehouse-prod.marketing.appsflyer_organic_installs -- organic app 
+union all
+-- paid downloads 
+select
+  Country_Code
+  , date(Event_Time) as download_date
+  , platform
+  , advertising_id 
+  , 'paid' as download_type
+from
+  etsy-data-warehouse-prod.marketing.appsflyer_paid_installs 
+) 
+select 
+  downloads.country_code
+  , downloads.download_date
+  , downloads.platform
+  , downloads.download_type
+  , segments.buyer_segment
+  , downloads.advertising_id
+from 
+  all_downloads downloads
+left join 
+  etsy-data-warehouse-prod.user_mart.mapped_user_profile segments 
+    on downloads.advertising_id=cast(segments.mapped_user_id as string)
