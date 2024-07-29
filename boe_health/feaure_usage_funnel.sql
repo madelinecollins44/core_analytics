bec87ec9affd8df8b12cc5551901ac49575a1edd
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-29 11:15:36 -0400

    Create feaure_usage_funnel.sql

---
+++ boe_health/feaure_usage_funnel.sql
@@ -0,0 +1,19 @@
+select
+  v.platform
+  , v.browser_platform
+	, v.region
+	, v.admin
+	, v.top_channel
+  , e.event_type
+  , count(distinct e.visit_id) as unique_visits
+  , count(e.visit_id) as total_visits 
+  , count(distinct v.user_id) as unique_users
+  , count (v.user_id) as total_users
+  , count(distinct v.browser_id) as unique_browsers
+  , count (v.browser_id) as total_browser_id
+from
+  etsy-data-warehouse-prod.weblog.visits v
+inner join
+  etsy-data-warehouse-prod.weblog.events e 
+    using (visit_id, _date)
+group by all 
