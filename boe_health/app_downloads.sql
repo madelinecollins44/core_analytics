49017d635ab5eacb97ae58e0c5c70f718011bad3
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-07-31 09:25:01 -0400

    Update app_downloads.sql

--- boe_health/app_downloads.sql
+++ boe_health/app_downloads.sql
@@ -1,126 +1,83 @@
-------------------------------------------------------------
---kristis method
-------------------------------------------------------------
-WITH first_app AS (
-  SELECT
-      user_id,
-      case when app_name = "ios-EtsyInc" then 'iOS'
-        when app_name = "android-EtsyInc" then 'Android'
-        else 'Unknown' end as OS,
+--this rollup looks at first boe visit ever from each user (regardless of the browser_id)
+with app_visits as (
+    SELECT DISTINCT
+      u.mapped_user_id,
+      v.visit_id,
       canonical_region,
-      min(run_date) AS first_app
-    FROM
-      `etsy-data-warehouse-prod`.weblog.visits
-    WHERE _date >= "2010-01-01"
-    and platform = "boe"
-    and app_name in ("ios-EtsyInc","android-EtsyInc")
-    and user_id > 0
-    -- filter out visits that start on the CC settings page
-    -- there was an attack inflating "downloads" in August 2021
-    and landing_event != "account_credit_card_settings"
-    GROUP BY all
-), yy_union AS (
+      browser_id,
+      browser_platform, 
+      platform,
+      -- v.event_source as os,
+      -- case
+      --   when app_name in ('ios-EtsyInc','android-EtsyInc') then 'boe'
+      --   else 'esa' end as app_type,
+      v.start_datetime,
+    FROM `etsy-data-warehouse-prod.weblog.visits` v
+    LEFT JOIN `etsy-data-warehouse-prod.user_mart.user_mapping` u on v.user_id = u.user_id -- want to also include anyone that was signed out during download time 
+    WHERE
+      v.event_source in ('ios','android')
+      -- AND v.app_name in ('ios-EtsyInc','android-EtsyInc','ios-ButterSellOnEtsy', 'android-ButterSellOnEtsy') -- only looking at boe downloads 
+      AND v._date >= '2022-01-01'
+      AND landing_event != "account_credit_card_settings" -- filter out visits that start on the CC settings page. there was an attack inflating "downloads" in August 2021
+      and platform in ('boe')
+  )
+  , first_visits as (
   SELECT
-      'ty' AS era,
-      DATE(timestamp_seconds(first_app.first_app)) AS date,
-      OS,
-      canonical_region,
-      count(*) AS users
-    FROM
-      first_app
-    GROUP BY all
-  UNION ALL
-  SELECT
-      'ly' AS era,
-      CAST(date_add(DATE(timestamp_seconds(first_app_0.first_app)), interval 52 WEEK) as DATETIME) AS date,
-      OS,
-      canonical_region,
-      count(*) AS users
-    FROM
-      first_app AS first_app_0
-    GROUP BY all
-  ORDER BY
-    1 NULLS LAST,
-    2, 3, 4
-)
-SELECT
-    yy_union.date,
-    OS,
-    canonical_region,
-    sum(CASE
-      WHEN yy_union.era = 'ty' THEN yy_union.users
-    END) AS ty_downloads,
-    sum(CASE
-      WHEN yy_union.era = 'ly' THEN yy_union.users
-    END) AS ly_downloads
-  FROM
-    yy_union
-  WHERE yy_union.date < CAST(current_date() as DATETIME)
-  GROUP BY all
-ORDER BY
-  1,2, 3
-
-------------------------------------------------------------
---using first download date
-------------------------------------------------------------
---find first boe visit 
-create or replace temporary table app_downloads as (
-with all_boe_visits as (
-select
- _date 
-  , visit_id
-  , user_id
-  , browser_id
-  , platform
-  , browser_platform
-  , top_channel
-  , region
-  , row_number() over (partition by user_id order by unix_seconds(timestamp (_date)) desc) AS visit_order
-from 
-  etsy-data-warehouse-prod.weblog.visits
-where 
-  platform in ('boe')
-and _date >= current_date-365
-)
-select
-  *
-from all_boe_visits 
-where visit_order =1
-); 
-
-------------------------------------------------------------
---using marketings app download tables
-------------------------------------------------------------
-with all_downloads as (
---organic installs 
-select
-  Country_Code
-  , date(Event_Time) as download_date
-  , platform
-  , advertising_id 
-  , 'organic' as download_type
-from
-  etsy-data-warehouse-prod.marketing.appsflyer_organic_installs -- organic app 
-union all
--- paid downloads 
-select
-  Country_Code
-  , date(Event_Time) as download_date
-  , platform
-  , advertising_id 
-  , 'paid' as download_type
-from
-  etsy-data-warehouse-prod.marketing.appsflyer_paid_installs 
-) 
-select 
-  downloads.country_code
-  , downloads.download_date
-  , downloads.platform
-  , downloads.download_type
-  , segments.buyer_segment
-  , downloads.advertising_id
-from 
-  all_downloads downloads
-left join 
-  etsy-data-warehouse-prod.user_mart.mapped_user_profile segments 
-    on downloads.advertising_id=cast(segments.mapped_user_id as string)
+    mapped_user_id,
+    buyer_segment,
+    case when mapped_user_id is not null then 1 else 0 end as signed_in,
+    app_type,
+    visit_id,
+    canonical_region,  
+    -- os,
+    browser_platform,
+    start_datetime,
+  FROM app_visits 
+  left join etsy-data-warehouse-prod.user_mart.mapped_user_profile using (mapped_user_id) 
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY mapped_user_id ORDER BY start_datetime) = 1 -- this only looks at first app for each USER, not each user across different devices 
+  ), yy_union as (
+    select
+    --ty calcs
+      date(timestamp(start_datetime)) as _date,
+      'ty' as era, 
+      buyer_segment, 
+      signed_in,
+      -- app_type,
+      canonical_region,  
+      -- os,
+      browser_platform,
+      count(distinct visit_id) as downloads,
+      count(distinct mapped_user_id) as user_downloads
+  from first_visits
+  group by all 
+  union all 
+  -- ly calcs 
+  select
+     date_add(date(timestamp(start_datetime)), interval 52 WEEK) as _date,
+     'ly' as era, 
+      buyer_segment, 
+      signed_in,
+      -- app_type,
+      canonical_region,  
+      -- os,
+      browser_platform,
+      count(distinct visit_id) as downloads,
+      count(distinct mapped_user_id) as user_downloads
+      from first_visits
+  group by all 
+  )
+  select
+    _date,
+    era, 
+    buyer_segment, 
+    signed_in,
+    -- app_type,
+    canonical_region,  
+    -- os,
+    browser_platform,
+    sum(case when era= 'ty' then downloads end) as ty_downloads_visit_level,
+    sum(case when era= 'ty' then user_downloads end) as ty_downloads_user_level,
+    sum(case when era= 'ly' then downloads end) as ty_downloads_visit_level,
+    sum(case when era= 'ly' then user_downloads end) as ty_downloads_user_level,
+  from yy_union
+  group by all 
