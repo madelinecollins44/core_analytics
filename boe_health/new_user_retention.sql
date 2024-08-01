83ac716dbd86661bff46385e56626f00343d05bd
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-08-01 13:42:33 -0400

    Update new_user_retention.sql

--- boe_health/new_user_retention.sql
+++ boe_health/new_user_retention.sql
@@ -1,3 +1,53 @@
+------------------------------------------------------------------------------------------------------------------------
+--USING VISITS W SEGMENTS: this way is wayyyy easier but slightly different buyer segments 
+------------------------------------------------------------------------------------------------------------------------  create or replace temp table first_visits as (
+  select
+  v.browser_id,
+  v.browser_platform,
+  v.region,
+  v._date as first_app_visit,
+  v.user_id,
+  -- coalesce(cast(v.user_id as string), v.browser_id) as unique_id, -- coalescing here so even if user is signed out, user_id = null, then browser will be counted 
+  s.buyer_segment,
+  v.event_source,
+  v.start_datetime,
+  case when v.user_id is not null then 1 else 0 end as is_signed_in,
+  lead(v._date) over (partition by v.browser_id order by v.start_datetime asc) as next_visit_date
+from 
+  `etsy-data-warehouse-prod.weblog.visits` v  
+left join 
+    etsy-data-warehouse-prod.rollups.visits_w_segments s using (user_id, visit_id)
+  where v.platform = "boe"
+  and v._date is not null 
+  and v.event_source in ("ios", "android")
+  group by all
+qualify row_number() over(partition by v.browser_id order by start_datetime desc) = 1
+);
+
+create or replace table etsy-data-warehouse-dev.rollups.boe_user_retention as (
+select 
+is_signed_in,
+browser_platform,
+region,
+buyer_segment,--segment when they downloaded the app
+count(distinct browser_id) as first_visit,
+count(distinct case when first_app_visit = next_visit_date then browser_id end) as same_day_visits,
+count(distinct case when next_visit_date <= first_app_visit + 6 then browser_id end) as first_7_days,
+count(distinct case when next_visit_date <= first_app_visit + 13 then browser_id end) as first_14_days,
+count(distinct case when next_visit_date <= first_app_visit + 29 then browser_id end) as first_30_days,
+--pct
+count(distinct case when first_app_visit = next_visit_date then browser_id end)/nullif(count(distinct browser_id),0) as pct_same_day_visits,
+count(distinct case when next_visit_date <= first_app_visit + 6 then browser_id end)/nullif(count(distinct browser_id),0) as pct_first_7_days,
+count(distinct case when next_visit_date <= first_app_visit + 13 then browser_id end)/nullif(count(distinct browser_id),0) as pct_first_14_days,
+count(distinct case when next_visit_date <= first_app_visit + 29 then browser_id end)/nullif(count(distinct browser_id),0) as pct_first_30_days
+from first_visits
+where first_app_visit <= current_date
+group by all
+);
+
+------------------------------------------------------------------------------------------------------------------------
+--USING MAPPED USER ID
+------------------------------------------------------------------------------------------------------------------------
 ----this rollup looks at the first visit for each user. if the user is signed out, then we use the browser_id
   begin
 
