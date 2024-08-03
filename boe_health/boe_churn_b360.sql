05991529c07818208283fc81a6f0176e94c008f6
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-08-02 23:09:09 -0400

    Update boe_churn_b360.sql

--- boe_health/boe_churn_b360.sql
+++ boe_health/boe_churn_b360.sql
@@ -1,6 +1,6 @@
 declare last_date date;
 
-drop table if exists `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`;
+-- drop table if exists `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`;
 
 create table if not exists `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`  (
     _date date
@@ -31,6 +31,9 @@ select
   , count(distinct case when last_boe_visit_date <= _date - 89 then mapped_user_id end) as user_visit_boe_in_three_months
   , count(distinct case when last_boe_visit_date <= _date - 179 then mapped_user_id end) as user_visit_boe_in_six_months
   , count(distinct case when last_boe_visit_date <= _date - 364 then mapped_user_id end) as user_visit_boe_in_year
+  --comparison to web metrics
+    , count(distinct case when last_boe_visit_date <= _date - 364 then mapped_user_id end) as user_visit_boe_in_year
+
 from 
   etsy-data-warehouse-prod.buyer360.buyer_ltd
 where 
