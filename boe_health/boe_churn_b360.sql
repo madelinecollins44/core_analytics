cdae46ac6ef5de02cdab321c096e238f0578822e
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-08-02 22:54:58 -0400

    Update boe_churn_b360.sql

--- boe_health/boe_churn_b360.sql
+++ boe_health/boe_churn_b360.sql
@@ -1,33 +1,40 @@
-with b360 as (
+declare last_date date;
+
+drop table if exists `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`;
+
+create table if not exists `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`  (
+    _date date
+    , users_visit_in_last_year int64
+    , user_visit_in_same_day int64
+    , user_visit_boe_in_last_week int64
+    , user_visit_boe_in_two_weeks int64
+    , user_visit_boe_in_three_weeks int64
+    , user_visit_boe_in_last_months int64
+    , user_visit_boe_in_three_months int64
+    , user_visit_boe_in_six_months int64
+    , user_visit_boe_in_year int64
+); 
+
+set last_date = (select max(_date) from `etsy-data-warehouse-dev.rollups.boe_churn_segmentation`);
+ if last_date is null then set last_date = (select min(_date)-1 from `etsy-data-warehouse-prod.buyer360.buyer_ltd`);
+ end if;
+
+insert into `etsy-data-warehouse-dev.rollups.boe_churn_segmentation` (
 select
   _date
-  , mapped_user_id
-  , last_boe_visit_date
-  -- , last_visit_date
-  -- , case when last_visit_date > last_boe_visit_date then 'last_visit_not_boe' else null end
+  , count(distinct case when last_visit_date >= current_date-365 then mapped_user_id end) as users_visit_in_last_year
+  , count(distinct case when last_boe_visit_date = _date then mapped_user_id end) as user_visit_in_same_day
+  , count(distinct case when last_boe_visit_date <= _date - 6 then mapped_user_id end) as user_visit_boe_in_last_week
+  , count(distinct case when last_boe_visit_date <= _date - 14 then mapped_user_id end) as user_visit_boe_in_two_weeks
+  , count(distinct case when last_boe_visit_date <= _date - 20 then mapped_user_id end) as user_visit_boe_in_three_weeks
+  , count(distinct case when last_boe_visit_date <= _date - 29 then mapped_user_id end) as user_visit_boe_in_last_month
+  , count(distinct case when last_boe_visit_date <= _date - 89 then mapped_user_id end) as user_visit_boe_in_three_months
+  , count(distinct case when last_boe_visit_date <= _date - 179 then mapped_user_id end) as user_visit_boe_in_six_months
+  , count(distinct case when last_boe_visit_date <= _date - 364 then mapped_user_id end) as user_visit_boe_in_year
 from 
   etsy-data-warehouse-prod.buyer360.buyer_ltd
 where 
-  _date >= current_date-5
-  and last_visit_date <= current_date-365 -- excludes any user that has not visited in a year 
-)
-select
-  a._date
-  , count(distinct case when a._date between date_sub(a._date, interval 1 year) and a._date then a.visit_id end) AS visits_last_year
-  , count(distinct case when b.last_boe_visit_date=a._date then mapped_user_id end) as users_visit_today
-  , count(distinct case when b.last_boe_visit_date between date_sub(a._date, interval 1 week) and a._date then mapped_user_id end) as users_visit_one_week
-  , count(distinct case when b.last_boe_visit_date between date_sub(a._date, interval 2 week) and a._date then mapped_user_id end) as users_visit_two_weeks
-  , count(distinct case when b.last_boe_visit_date between date_sub(a._date, interval 3 week) and a._date then mapped_user_id end) as users_visit_three_week
-  , count(distinct case when b.last_boe_visit_date between date_sub(a._date, interval 1 month) and a._date then mapped_user_id end) as users_visit_one_month
-  , count(distinct case when b.last_boe_visit_date between date_sub(a._date, interval 3 month) and a._date then mapped_user_id end) as users_visit_three_months
-  , count(distinct case when b.last_boe_visit_date between date_sub(a._date, interval 6 month) and a._date then mapped_user_id end) as users_visit_six_months
-  , count(distinct case when b.last_boe_visit_date between date_sub(a._date, interval 1 year) and a._date then mapped_user_id end) as users_visit_one_year
-from 
-  etsy-data-warehouse-prod.weblog.visits a
-left join 
-  b360 b 
-    on a._date=b._date
-where 
-  a._date >= current_date-5
-  and a.platform in ('boe')
+  _date >= last_date
+  and last_visit_date >= current_date-365 -- excludes any user that has not visited in a year 
 group by all
+); 
