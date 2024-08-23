764208fd1f5f27004bcd3b143d8337f8f1dfa8e0
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-08-23 14:00:47 -0400

    Update wau_mau_retention.sql

--- boe_health/wau_mau_retention.sql
+++ boe_health/wau_mau_retention.sql
@@ -5,32 +5,68 @@ create or replace table etsy-data-warehouse-dev.rollups.boe_waus_retention as (
 with waus as (
   select 
     date_trunc(_date, week) as week,
-    v.user_id,
-    count(*) as visits,
-    sum(total_gms) as gms
-  from `etsy-data-warehouse-prod.weblog.visits` v  
+    -- buyer_segment,
+    top_channel,
+    browser_platform,
+    region,
+    case when v.user_id is not null then 1 else 0 end as signed_in,
+    b.mapped_user_id,
+    count(distinct v.visit_id) as visits,
+    sum(v.total_gms) as gms,
+  from 
+    etsy-data-warehouse-prod.weblog.visits v 
+  left join  
+    etsy-data-warehouse-prod.user_mart.user_mapping b 
+      on v.user_id=b.user_id 
+  -- left join 
+  --   buyer_segment s 
+  --     on b.mapped_user_id=s.mapped_user_id
+  --     and v_date= s.as_of_date
   where platform = "boe"
-    and _date >= "2019-12-01"
-    and canonical_region = "US"
+    and _date >= "2024-06-01"
     and _date != "2024-02-29"
-    and event_source = "ios"
-    and v.user_id > 0
   group by all
-  ),
-next_visit_weeks as (
-  select *,
-    lead(week) over (partition by user_id order by week asc) as next_visit_week 
-  from waus 
   )
-select 
-  week,
-  count(*) as waus,
-  count(case when next_visit_week = date_add(week, interval 1 week) then user_id end) as retained,
-  count(case when next_visit_week = date_add(week, interval 1 week) then user_id end) / count(*) as pct_retained,
+-- next, grab the visit info that appears the most for each mapped_user_id. since its tracking user_ids, we cannot track signed out 
+, common_visit_info as (
+select
+  --buyer_segment,
+  top_channel,
+  browser_platform,
+  region,
+  signed_in,
+  mapped_user_id,
+  visits,
+from waus
+qualify row_number() over(partition by mapped_user_id order by visits) = 1
+)
+--now get the visits from the following week for each mapped user id
+, next_visit_week as (
+select  
+  week, 
+  visits, 
+  gms,
+  mapped_user_id, 
+  lead(week) over (partition by mapped_user_id order by week asc) as next_visit_week 
+from waus 
+)
+  select
+  nw.week,
+  -- vi.buyer_segment, 
+  vi.top_channel,
+  vi.browser_platform,
+  vi.region,
+  vi.signed_in,
+  count(nw.mapped_user_id) as waus, 
+  count(case when nw.next_visit_week = date_add(week, interval 1 week) then mapped_user_id end) as retained,
+  count(case when nw.next_visit_week = date_add(week, interval 1 week) then mapped_user_id end) / count(*) as pct_retained,
   sum(gms) as gms
-from next_visit_weeks 
-group by all
-order by 1
+from 
+  next_visit_week nw 
+left join 
+  common_visit_info vi
+    using (mapped_user_id)
+group by all 
 ); 
 
 -- MAU Retention
@@ -38,30 +74,66 @@ create or replace table etsy-data-warehouse-dev.rollups.boe_maus_retention as (
 with maus as (
   select 
     date_trunc(_date, month) as month,
-    v.user_id,
-    count(*) as visits,
-    sum(total_gms) as gms
-  from `etsy-data-warehouse-prod.weblog.visits` v  
+    -- buyer_segment,
+    top_channel,
+    browser_platform,
+    region,
+    case when v.user_id is not null then 1 else 0 end as signed_in,
+    b.mapped_user_id,
+    count(distinct v.visit_id) as visits,
+    sum(v.total_gms) as gms,
+  from 
+    etsy-data-warehouse-prod.weblog.visits v 
+  left join  
+    etsy-data-warehouse-prod.user_mart.user_mapping b 
+      on v.user_id=b.user_id 
+  -- left join 
+  --   buyer_segment s 
+  --     on b.mapped_user_id=s.mapped_user_id
+  --     and v_date= s.as_of_date
   where platform = "boe"
-    and _date >= "2019-12-01"
-    and canonical_region = "US"
+    and _date >= "2024-06-01"
     and _date != "2024-02-29"
-    and event_source = "ios"
-    and v.user_id > 0
   group by all
-  ),
-next_visit_months as (
-  select *,
-    lead(month) over (partition by user_id order by month asc) as next_visit_month
-  from waus 
   )
-select 
-  month,
-  count(*) as maus,
-  count(case when next_visit_month = date_add(month, interval 1 month) then user_id end) as retained,
-  count(case when next_visit_month = date_add(month, interval 1 month) then user_id end) / count(*) as pct_retained,
+-- next, grab the visit info that appears the most for each mapped_user_id. since its tracking user_ids, we cannot track signed out 
+, common_visit_info as (
+select
+  --buyer_segment,
+  top_channel,
+  browser_platform,
+  region,
+  signed_in,
+  mapped_user_id,
+  visits,
+from maus
+qualify row_number() over(partition by mapped_user_id order by visits) = 1
+)
+--now get the visits from the following month for each mapped user id
+, next_visit_month as (
+select  
+  month, 
+  visits, 
+  gms,
+  mapped_user_id, 
+  lead(month) over (partition by mapped_user_id order by month asc) as next_visit_month 
+from maus 
+)
+  select
+  nw.month,
+  -- vi.buyer_segment, 
+  vi.top_channel,
+  vi.browser_platform,
+  vi.region,
+  vi.signed_in,
+  count(nw.mapped_user_id) as maus, 
+  count(case when nw.next_visit_month = date_add(month, interval 1 month) then mapped_user_id end) as retained,
+  count(case when nw.next_visit_month = date_add(month, interval 1 month) then mapped_user_id end) / count(*) as pct_retained,
   sum(gms) as gms
-from next_visit_months 
-group by all
-order by 1
+from 
+  next_visit_month nw 
+left join 
+  common_visit_info vi
+    using (mapped_user_id)
+group by all 
 ); 
