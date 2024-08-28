21a7460b3a0a85b4a6d948d9685a922d4955d2d8
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-08-28 17:03:56 -0400

    Create boe_email_stats.sql

---
+++ boe_health/boe_email_stats.sql
@@ -0,0 +1,37 @@
+create or replace table `etsy-data-warehouse-dev.madelinecollins.boe_email_stats` as (
+with boe_users as (
+  select 
+    distinct user_id,
+  from etsy-data-warehouse-prod.buyer360.buyer_ltd
+  where boe_app_visits > 0
+)
+, email_subscriptions as (
+select
+  utm_source
+  , count(distinct a.user_id) as boe_subscribers 
+  , count(distinct b.user_id) as total_subscribers 
+from 
+  etsy-data-warehouse-prod.rollups.email_subscribers b
+left join 
+  boe_users a  using (user_id)
+group by all 
+)
+select 
+  d.utm_source
+  , count(distinct d.euid) as delivered
+  , count(distinct o.euid) as opens
+  , count(distinct c.euid) as clicks
+from 
+  boe_users u
+left join 
+  etsy-data-warehouse-prod.mail_mart.delivered d using (user_id)
+left join 
+  etsy-data-warehouse-prod.mail_mart.opens o
+    on d.user_id = o.user_id
+    and d.euid=o.euid
+left join etsy-data-warehouse-prod.mail_mart.clicks c
+    on c.user_id = o.user_id
+    and c.euid=o.euid
+where date(timestamp_seconds(d.send_date)) >= current_date-365
+group by all
+);
