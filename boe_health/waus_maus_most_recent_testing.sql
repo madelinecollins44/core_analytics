154deb9c69859197de1647f8bc5b78ac6a31a657
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-08-28 17:03:39 -0400

    Update waus_maus_most_recent_testing.sql

--- boe_health/waus_maus_most_recent_testing.sql
+++ boe_health/waus_maus_most_recent_testing.sql
@@ -5,3 +5,12 @@ select * from  etsy-data-warehouse-dev.rollups.boe_maus_retention where mapped_u
 -- 2024-07-01	Active	direct	iOS	FR	929317673	1	0		
 -- 2024-06-01	Active	direct	iOS	FR	929317673	1	1		
 -- 2024-05-01	Active	direct	iOS	FR	929317673	1	1		
+
+--------
+select week, sum(ty_waus) as ty_waus, sum(ty_retained) as ty_retained, sum(ly_waus) as ly_waus, sum(ly_retained) as ly_retained 
+  from etsy-data-warehouse-dev.rollups.boe_waus_retention 
+  where week = '2024-08-25' or week= '2023-08-27' 
+  group by all 
+-- week	ty_waus	ty_retained	ly_waus	ly_retained
+-- 2023-08-27	16874082	9966875	14957418	8980775
+-- 2024-08-25	10599418	0	16874082	9966875
