2e900ad3e11f65dfcf3535ba2b25c42468569a02
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-09-10 17:16:15 -0400

    Create boe_push_comparisons.sql

---
+++ boe_refresh/boe_push_comparisons.sql
@@ -0,0 +1,7 @@
+  --  QUESTION TO ANSWER: Look into what types of users we’re driving to the app via push, and how their experiences differ onsite
+
+
+-- how many users come from push?
+-- what does engagement look like by push type, push vs not push (conversion rate, listing views, exit rate, engagement rate, types of engagement, acvv)?
+-- reactivated users between push types, push vs not push 
+-- buyer segments and push engagements 
