52812c923c63dc2a6699cf1d3f96c8e2732f2a2d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-10 09:47:59 -0600

    Update login_event_next_screen.sql

--- boe_onboarding/login_event_next_screen.sql
+++ boe_onboarding/login_event_next_screen.sql
@@ -131,3 +131,84 @@ select
   count(distinct case when event_name in ('BOE_etsy_sign_in_tapped') then browser_id end) as etsy_sign_ins
 from all_signin_events
 
+------------------------------------------------------------------------------------
+--order of screens browsers go to, how many go from register to sign in?
+------------------------------------------------------------------------------------
+with grouped_screens as (
+  select 
+  a.browser_id,
+  b.visit_id,
+  b.sequence_number,
+  case when event_name in (
+        'sign_in_screen',
+        'continue_as_guest_tapped',
+        'login_view',
+        'BOE_social_sign_in_tapped',
+        'BOE_etsy_sign_in_tapped'
+      ) then '1 - Log In Splash Screen'
+      when event_name  IN (
+        'join_submit',
+        'BOE_email_sign_in_webview_cancelled'
+      ) then '2 - Registration Web View'
+      when event_name  IN (
+        'signin_submit',
+        'magic_link_click',
+        'magic_link_send',
+        'magic_link_redeemed',
+        'magic_link_error',
+        'forgot_password',
+        'forgot_password_view',
+        'forgot_password_email_sent',
+        'reset_password',
+        'keep_me_signed_in_checked',
+        'keep_me_signed_in_unchecked',
+        'BOE_email_sign_in_webview_cancelled'
+      ) then '3 - Sign In Web View'
+      when event_name  IN (
+        'login_view',
+        'BOE_social_sign_in_tapped'
+      ) then '4 - Sign In from Homescreen'
+      when event_name  IN (
+        'notification_registration_interstitial_enable_tapped',
+        'notification_registration_interstitial_dismiss_tapped',
+        'update_setting',
+        'notification_registration_interstitial_viewed'
+      ) then '5 - Notifications Opt In pt 1'
+      end as screen_name
+  from etsy-data-warehouse-dev.madelinecollins.boe_first_visits a
+  inner join etsy-data-warehouse-dev.madelinecollins.app_onboarding_events b using (visit_id)
+  where visit_rnk = 1 --pull out first visit 
+    and _date >= current_date-30
+    and event_source in ('ios') -- only looking at ios 
+)
+ , first_screen_events as (
+  -- Assign the first sequence number to each screen
+  select
+    browser_id,
+    screen_name,
+    min(sequence_number) as first_sequence_number
+  from
+    grouped_screens
+  group by all 
+), lead_lag_screens AS (
+  -- Create lead and lag views to track screen transitions
+  select
+    browser_id,
+    screen_name,
+    first_sequence_number,
+    lag(screen_name) over (partition by browser_id order by first_sequence_number) as previous_screen,
+    lead(screen_name) over (partition by browser_id order by  first_sequence_number) as next_screen
+  from
+    first_screen_events
+)
+-- Final selection, ordered by sequence number
+select
+  browser_id,
+  first_sequence_number,
+  screen_name,
+  previous_screen,
+  next_screen
+from
+  lead_lag_screens
+order by 
+  browser_id, first_sequence_number;
