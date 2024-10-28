a7b3b83990cd2c5b3df11d2ef0eddb6fc6521fcc
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-28 10:10:05 -0400

    Update page_usage.sql

--- shop_home_analysis/page_usage.sql
+++ shop_home_analysis/page_usage.sql
@@ -2,6 +2,30 @@
 --What share of visits & GMS flow through Shop Home today?  Overall and by platform
 ----include YY comparison
 ----------------------------------------------------------------------------------------------------
+with shop_home_visits as (
+select distinct
+  visit_id
+from 
+  etsy-data-warehouse-prod.weblog.events
+where 
+  event_type in ('shop_home')
+  and _date>= current_date-30
+)
+select
+  v._date
+  , count(distinct v.visit_id) as total_visits
+  , count(distinct shv.visit_id) as shop_home_visits
+  , count(distinct shv.visit_id)/count(distinct v.visit_id) as share_of_visits
+  , sum(v.total_gms) as total_gms
+  , sum(case when shv.visit_id is not null then v.total_gms end) as shop_home_gms
+  , sum(case when shv.visit_id is not null then v.total_gms end)/sum(case when shv.visit_id is not null then v.total_gms end) as share_of_gms
+from 
+  etsy-data-warehouse-prod.weblog.visits v
+left join 
+  shop_home_visits shv using (visit_id)
+where v._date >= current_date-30
+group by all 
+
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
 --Which types of buyers go to shop home? 
