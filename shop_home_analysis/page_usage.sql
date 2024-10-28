db2f980ac42a74f04209aa7a18d58e25529d88c7
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-28 11:37:15 -0400

    Update page_usage.sql

--- shop_home_analysis/page_usage.sql
+++ shop_home_analysis/page_usage.sql
@@ -13,7 +13,17 @@ where
    and page_view =1 
 group by all
 order by 2 desc
-
+-- view_listing, 580961218
+-- homescreen, 166773892
+-- shop_home, 151715149
+-- home, 142581209
+-- recommended, 110865610
+-- market, 98550584
+-- listing_page_recommendations, 91328633
+--cart_view, 81441882
+--search, 79115151
+--browselistings, 60008060
+-- autosuggest, 51274396
   
 ----compare to most popular pages
 with event_visits as (
@@ -23,7 +33,7 @@ select distinct
 from 
   etsy-data-warehouse-prod.weblog.events
 where 
-  event_type in ('view_listing','shop_home','cart_view','home','homescreen','thank_you','search','your_purchases')
+  event_type in ('view_listing','shop_home','cart_view','home','homescreen','recommended','search','market')
   and _date>= current_date-30
 )
 select
@@ -33,23 +43,24 @@ select
   count(distinct case when event_type in ('shop_home') then ev.visit_id end) as shop_home_visits, 
   count(distinct case when event_type in ('cart_view') then ev.visit_id end) as cart_view_visits, 
   count(distinct case when event_type in ('home','homescreen') then ev.visit_id end) as home_visits, 
-  count(distinct case when event_type in ('thank_you') then ev.visit_id end) as thank_you_visits, 
+  count(distinct case when event_type in ('recommended') then ev.visit_id end) as recommended_visits, 
   count(distinct case when event_type in ('search') then ev.visit_id end) as search_visits, 
-  count(distinct case when event_type in ('your_purchases') then ev.visit_id end) as your_purchases_visits, 
+  count(distinct case when event_type in ('market') then ev.visit_id end) as market_visits, 
 
   sum(v.total_gms) as total_gms, 
   sum(case when event_type in ('view_listing') and ev.visit_id is not null then v.total_gms end) as view_listing_gms, 
   sum(case when event_type in ('shop_home') and ev.visit_id is not null then v.total_gms end) as shop_home_gms, 
   sum(case when event_type in ('cart_view') and ev.visit_id is not null then v.total_gms end) as cart_view_gms, 
   sum(case when event_type in ('home','homescreen') and ev.visit_id is not null then v.total_gms end) as home_gms, 
-  sum(case when event_type in ('thank_you') and ev.visit_id is not null then v.total_gms end) as thank_you_gms, 
+  sum(case when event_type in ('recommended') and ev.visit_id is not null then v.total_gms end) as recommended_gms, 
   sum(case when event_type in ('search') and ev.visit_id is not null then v.total_gms end) as search_gms, 
-  sum(case when event_type in ('your_purchases') and ev.visit_id is not null then v.total_gms end) as your_purchases_gms, 
+  sum(case when event_type in ('market') and ev.visit_id is not null then v.total_gms end) as market_gms, 
 from 
   etsy-data-warehouse-prod.weblog.visits v
 left join event_visits ev using (visit_id)
 where v._date >= current_date-30
 group by all 
+
 
 ----look at agg over last 30 days, platform
 with shop_home_visits as (
