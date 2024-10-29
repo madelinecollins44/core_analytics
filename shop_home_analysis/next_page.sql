89eab975bb546397ff42c6d4e9c65ba316e43cf4
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-29 14:22:12 -0400

    Update next_page.sql

--- shop_home_analysis/next_page.sql
+++ shop_home_analysis/next_page.sql
@@ -86,3 +86,31 @@ where
   _date >= current_date-30
 group by all 
 order by 4 desc
+
+--entire page jounrey
+with shop_home_visits as (
+select
+  visit_id,
+  sequence_number,
+  event_type,
+  lead(event_type) over (partition by visit_id order by sequence_number) as next_page,
+  lag(event_type) over (partition by visit_id order by sequence_number) as previous_page
+from 
+  etsy-data-warehouse-prod.weblog.events
+where
+  _date >= current_date-30
+  and page_view=1 
+)
+select
+  next_page,
+  previous_page,
+  count(distinct visit_id) as visits
+from 
+  shop_home_visits shv
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v using (visit_id)
+where 
+  event_type in ('shop_home')
+  and v._date >= current_date-30
+group by all 
+order by 2 desc 
