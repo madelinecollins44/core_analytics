c6f987ba12cb841b866ba497c6bacfb0171375c9
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-29 13:52:16 -0400

    Update previous_page.sql

--- shop_home_analysis/previous_page.sql
+++ shop_home_analysis/previous_page.sql
@@ -51,11 +51,3 @@ where
   and v._date >= current_date-30
 group by all 
 order by 2 desc 
-
---how many listings views come from shop home?
-select
-  count(listing_id) as listing_views,
-  count(case when referring_page_event in ('shop_home') then listing_id end) as shop_home_listing_views
-from etsy-data-warehouse-prod.analytics.listing_views
-where _date >= current_date-30
-group by all  
