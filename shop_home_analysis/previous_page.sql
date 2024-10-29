edbe1177d6a022f0382196077c8237d327c217ce
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-29 14:30:49 -0400

    Update previous_page.sql

--- shop_home_analysis/previous_page.sql
+++ shop_home_analysis/previous_page.sql
@@ -54,9 +54,29 @@ order by 2 desc
 
 --where do landings come from?
 select  
-  top_channel,
+case 
+      when top_channel in ('direct') then 'Direct'
+      when top_channel in ('dark') then 'Dark'
+      when top_channel in ('internal') then 'Internal'
+      when top_channel in ('seo') then 'SEO'
+      when top_channel like 'social_%' then 'Non-Paid Social'
+      when top_channel like 'email%' then 'Email'
+      when top_channel like 'push_%' then 'Push'
+      when top_channel in ('us_paid','intl_paid') then
+        case
+          when (second_channel like '%gpla' or second_channel like '%bing_plas') then 'PLA'
+          when (second_channel like '%_ppc' or second_channel like 'admarketplace') then case
+          when third_channel like '%_brand' then 'SEM - Brand' else 'SEM - Non-Brand'
+          end
+      when second_channel='affiliates' then 'Affiliates'
+      when (second_channel like 'facebook_disp%' or second_channel like 'pinterest_disp%') then 'Paid Social'
+      when second_channel like '%native_display' then 'Display'
+      when second_channel in ('us_video','intl_video') then 'Video' else 'Other Paid' end
+      else 'Other Non-Paid' 
+      end as reporting_channel,
   count(distinct visit_id) as visits
 from etsy-data-warehouse-prod.weblog.visits
 where _date >= current_date-30
 and landing_event in ('shop_home')
+group by all 
 
