13c8ca7c736e6d7b73fc213028d1e7dc57c5ec3d
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-29 13:43:59 -0400

    Update previous_page.sql

--- shop_home_analysis/previous_page.sql
+++ shop_home_analysis/previous_page.sql
@@ -1,1 +1,35 @@
-aeg
+-----------------------------------------------------------------------------------------------------------------------------------------------
+--Where do they go after shop home?  
+----Prior screen, segment by visitors that purchase in-session vs. not, purchase something from the shop vs. not
+---------------------------------------------------------------------------------------------------------------------------------------------
+--overall traffic from shop home
+with shop_home_visits as (
+select
+  visit_id,
+  sequence_number,
+  event_type,
+  lag(event_type) over (partition by visit_id order by sequence_number) as previous_page
+from 
+  etsy-data-warehouse-prod.weblog.events
+where
+  _date >= current_date-30
+  and page_view=1 
+)
+select
+  previous_page,
+  count(distinct visit_id) as visits
+from 
+  shop_home_visits
+where 
+  event_type in ('shop_home')
+group by all 
+order by 2 desc 
+
+
+--how many listings views come from shop home?
+select
+  count(listing_id) as listing_views,
+  count(case when referring_page_event in ('shop_home') then listing_id end) as shop_home_listing_views
+from etsy-data-warehouse-prod.analytics.listing_views
+where _date >= current_date-30
+group by all  
