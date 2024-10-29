2e8eb81082fce87353b1a5984dc363014299318a
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-29 13:46:51 -0400

    Update previous_page.sql

--- shop_home_analysis/previous_page.sql
+++ shop_home_analysis/previous_page.sql
@@ -25,6 +25,32 @@ where
 group by all 
 order by 2 desc 
 
+--visits that convert 
+with shop_home_visits as (
+select
+  visit_id,
+  sequence_number,
+  event_type,
+  lag(event_type) over (partition by visit_id order by sequence_number) as previous_page
+from 
+  etsy-data-warehouse-prod.weblog.events
+where
+  _date >= current_date-30
+  and page_view=1 
+)
+select
+  previous_page,
+  count(distinct visit_id) as visits
+from 
+  shop_home_visits shv
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v using (visit_id)
+where 
+  event_type in ('shop_home')
+  and v.converted > 0 
+  and v._date >= current_date-30
+group by all 
+order by 2 desc 
 
 --how many listings views come from shop home?
 select
