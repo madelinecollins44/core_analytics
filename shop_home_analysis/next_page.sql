1c57ddfd421ca3bc40c04cd4d40d012afe623a23
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-29 13:42:39 -0400

    Update next_page.sql

--- shop_home_analysis/next_page.sql
+++ shop_home_analysis/next_page.sql
@@ -1,3 +1,30 @@
+---------------------------------------------------------------------------------------------------------------------------------------------
+--Where do they go after shop home?
+----Next screen, segment by visitors that purchase in-session vs. not, purchase something from the shop vs. not
+---------------------------------------------------------------------------------------------------------------------------------------------
+--overall traffic to shop home
+with shop_home_visits as (
+select
+  visit_id,
+  sequence_number,
+  event_type,
+  lead(event_type) over (partition by visit_id order by sequence_number) as previous_page
+from 
+  etsy-data-warehouse-prod.weblog.events
+where
+  _date >= current_date-30
+  and page_view=1 
+)
+select
+  previous_page,
+  count(distinct visit_id) as visits
+from 
+  shop_home_visits
+where 
+  event_type in ('shop_home')
+group by all 
+order by 2 desc 
+  
 --next page testing 
 with shop_home_visits as (
 select
