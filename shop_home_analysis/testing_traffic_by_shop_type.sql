46ce3bddccbdeaefb085141e9090785229a7a5bd
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-05 16:19:54 -0500

    Update testing_traffic_by_shop_type.sql

--- shop_home_analysis/testing_traffic_by_shop_type.sql
+++ shop_home_analysis/testing_traffic_by_shop_type.sql
@@ -63,3 +63,71 @@ from
 left join 
   shop_tiers b using (raw_shop_shop_id)
 group by all 
+
+----------TESTING NON MATCHING SHOP_IDS
+with shop_tiers as (
+select
+  vs.raw_shop_shop_id,
+  sb.seller_tier_new,
+  sb.power_shop_status,
+  sb.top_shop_status,
+  sb.medium_shop_status,
+  sb.small_shop_status
+from 
+  (select distinct raw_shop_shop_id from etsy-data-warehouse-dev.madelinecollins.visited_shop_ids) vs
+left join 
+  etsy-data-warehouse-prod.rollups.seller_basics sb 
+    on vs.raw_shop_shop_id= cast(sb.shop_id as string)
+group by all
+)
+--need to get shop_ids to visit level
+, pageviews_per_shop as (
+select
+  raw_shop_shop_id,
+  visit_id,
+  count(sequence_number) as pageviews
+from 
+  etsy-data-warehouse-dev.madelinecollins.visited_shop_ids
+group by all
+)
+, add_in_gms as (
+select
+  a.raw_shop_shop_id,
+  -- b.browser_platform,
+  -- platform,
+  a.visit_id,
+  a.pageviews,
+  sum(b.total_gms) as total_gms
+from 
+  pageviews_per_shop a
+inner join 
+  etsy-data-warehouse-prod.weblog.visits b using (visit_id)
+where 
+  _date >= current_date-30
+group by all 
+)
+, visit_level_metrics as (
+select
+  raw_shop_shop_id,
+  -- browser_platform,
+  -- platform,
+  count(distinct visit_id) as unique_visits,
+  sum(pageviews) as pageviews,
+  sum(total_gms) as total_gms,
+from add_in_gms
+group by all 
+)
+select
+  seller_tier_new,
+  a.raw_shop_shop_id
+from 
+  visit_level_metrics a
+left join 
+  shop_tiers b using (raw_shop_shop_id)
+where seller_tier_new is null
+and regexp_contains(raw_shop_shop_id, r'^\d+$') 
+group by all 
+--101721
+--888888888
+--2525259608
+--290722
