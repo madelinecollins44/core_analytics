d3cbbb80836407a9fd3d5cc0b14c55b3700d1e6b
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-06 14:08:26 -0500

    Update next_page.sql

--- shop_home_analysis/next_page.sql
+++ shop_home_analysis/next_page.sql
@@ -1,8 +1,9 @@
----------------------------------------------------------------------------------------------------------------------------------------------
 --Where do they go after shop home?
 ----Next screen, segment by visitors that purchase in-session vs. not, purchase something from the shop vs. not
+
 ---------------------------------------------------------------------------------------------------------------------------------------------
---overall traffic to shop home
+--overall traffic
+---------------------------------------------------------------------------------------------------------------------------------------------
 with shop_home_visits as (
 select
   visit_id,
@@ -26,7 +27,9 @@ where
 group by all 
 order by 2 desc 
 
---visits that convert 
+---------------------------------------------------------------------------------------------------------------------------------------------
+--visits that convert
+---------------------------------------------------------------------------------------------------------------------------------------------
 with shop_home_visits as (
 select
   visit_id,
@@ -54,7 +57,9 @@ where
 group by all 
 order by 2 desc 
 
---next page testing 
+---------------------------------------------------------------------------------------------------------------------------------------------
+--testing next page
+----------------------------------------------------------------------------------------------------
 with shop_home_visits as (
 select
   visit_id,
@@ -94,7 +99,9 @@ select * from shop_home_visits where visit_id in ('Aa4HjNXASTuQllHw7jRVX-EkvKkO.
 -- group by all 
 -- order by 2 desc 
 
----purchase rate of listings viewed from shop_home
+---------------------------------------------------------------------------------------------------------------------------------------------
+--purchase rate of listings from shop home
+---------------------------------------------------------------------------------------------------------------------------------------------
 select
   referring_page_event,
   count(listing_id) as listing_views,
@@ -118,8 +125,10 @@ where
 group by all 
 order by 4 desc
 
+---------------------------------------------------------------------------------------------------------------------------------------------
 --entire page jounrey
-with shop_home_visits as (
+---------------------------------------------------------------------------------------------------------------------------------------------
+	with shop_home_visits as (
 select
   visit_id,
   sequence_number,
@@ -146,8 +155,9 @@ where
 group by all 
 order by 2 desc 
 
-
+---------------------------------------------------------------------------------------------------------------------------------------------
 --next page of visits that have purchased from the shop itself
+---------------------------------------------------------------------------------------------------------------------------------------------
 --get visit info of when a visit see a shop_home page
 with purchased_from_shops as (
 select
@@ -200,3 +210,99 @@ from visits_to_home_and_purchase vh
 inner join next_page np using (visit_id, sequence_number)
 group by all
 
+---------------------------------------------------------------------------------------------------------------------------------------------
+--testing of purchase  visit
+---------------------------------------------------------------------------------------------------------------------------------------------
+--get visit info of when a visit see a shop_home page
+with purchased_from_shops as (
+select
+  tv.visit_id, 
+	t.seller_user_id,
+	cast(sb.shop_id as string) as shop_id,
+  count(transaction_id) as transactions
+from 
+  etsy-data-warehouse-prod.transaction_mart.transactions_visits tv
+inner join
+	etsy-data-warehouse-prod.transaction_mart.all_transactions t 
+		using (transaction_id)
+left join etsy-data-warehouse-prod.rollups.seller_basics sb
+	on t.seller_user_id=sb.user_id
+where tv.date >= current_date-30
+group by all 
+)
+-- visit_id	seller_user_id	shop_id	transactions
+-- FzzfQD6oSVWA0akkgMsckg.1729683003991.3	857550370	48131995	2 ---> DIDNT VISIT
+-- 6sTdZyEr2cR8seFr2R71rBelF9no.1730266340471.1	46996441	35942283	1
+-- pIRpDtpczJyPhy0uCyA7hld3QSbs.1729958036513.3	148655008	17516993	2
+-- 4m45rQuJ_fsD_lzb2tJN4JU5OCXd.1730403701095.3	589707121	33652197	7
+-- NjCKL1_ERbS7UZjQsR9aIg.1730388118286.1	556065882	36880361	1
+
+--find visits that have purchased from store, and when they visited the store within that visit 
+, visits_to_home_and_purchase as (
+select
+ b.visit_id,
+ b.sequence_number, -- need this so can join to next page
+ b.shop_id,
+ case when a.transactions is not null then 1 else 0 end
+from 
+  etsy-data-warehouse-dev.madelinecollins.visited_shop_ids b 
+left join 
+  purchased_from_shops a using (visit_id, shop_id)
+group by all
+)
+select * from purchased_from_shops where visit_id in ('Y9m3qVDsH1JusgXUtHayvEC6Q26l.1729682073017.2','0WHikVi8PBzqMS24QHK_XE6nPN6j.1728406555941.2	530	29669151','sNEDNrnE6MtmjsgpeXy7RrwzveZ2.1730082341929.1','6h4mQRv2EazdlhEyati0-WUExjml.1729483820853.2','A-NLqm4OiwWC19qkzp0R8YjjL-3L.1730770007674.1') and shop_id in ('17970214','29669151','7957168','40557450','51953130')
+--find visits with purchased 
+, visits_to_home_and_purchase as (
+select
+ b.visit_id,
+ b.sequence_number, -- need this so can join to next page
+ b.shop_id,
+ case when a.transactions is not null then 1 else 0 end as transactions
+from 
+  etsy-data-warehouse-dev.madelinecollins.visited_shop_ids b 
+left join 
+  purchased_from_shops a using (visit_id, shop_id)
+group by all
+)
+select * from visits_to_home_and_purchase where transactions > 0 limit 5  
+-----------the following are all NOT in the purchased table
+-- visit_id	sequence_number	shop_id	f0_
+-- Y9m3qVDsH1JusgXUtHayvEC6Q26l.1729682073017.2	349	17970214	0
+-- 0WHikVi8PBzqMS24QHK_XE6nPN6j.1728406555941.2	530	29669151	0
+-- sNEDNrnE6MtmjsgpeXy7RrwzveZ2.1730082341929.1	0	6803186	0
+-- 6h4mQRv2EazdlhEyati0-WUExjml.1729483820853.2	0	7957168	0
+-- A-NLqm4OiwWC19qkzp0R8YjjL-3L.1730770007674.1	79	51953130	0
+
+-----------the following are all in the purchased table
+-- NjCKL1_ERbS7UZjQsR9aIg.1730388118286.1	556065882	36880361	1
+-- 63F0564E903242B2A0B02D3EB72D.1728410365885.1	469	53551553	1
+-- _p7YpLub21-cd8YbnckDJWN2XnmV.1729194431120.1	250	46670827	1
+-- _YhuBqXLjqMhOGB8dQ3KgwmEMxnO.1730741363304.2	255	40557450	1
+-- C3B5436F04734D698E701A257B41.1730313226888.1	120	41991232	1
+-- D525975BBD664DD08DEF67C49DB1.1728665975144.1	837	50444114	1
+
+	
+---testing to make sure purchased are in that table
+with purchased_from_shops as (
+select
+  tv.visit_id, 
+	t.seller_user_id,
+	cast(sb.shop_id as string) as shop_id,
+  count(transaction_id) as transactions
+from 
+  etsy-data-warehouse-prod.transaction_mart.transactions_visits tv
+inner join
+	etsy-data-warehouse-prod.transaction_mart.all_transactions t 
+		using (transaction_id)
+left join etsy-data-warehouse-prod.rollups.seller_basics sb
+	on t.seller_user_id=sb.user_id
+where tv.date >= current_date-30
+group by all 
+)
+select * from purchased_from_shops where visit_id in ('NjCKL1_ERbS7UZjQsR9aIg.1730388118286.1','63F0564E903242B2A0B02D3EB72D.1728410365885.1','_p7YpLub21-cd8YbnckDJWN2XnmV.1729194431120.1','_YhuBqXLjqMhOGB8dQ3KgwmEMxnO.1730741363304.2','C3B5436F04734D698E701A257B41.1730313226888.1','D525975BBD664DD08DEF67C49DB1.1728665975144.1') and shop_id in ('36880361','53551553','46670827','40557450','41991232','50444114')
+-- NjCKL1_ERbS7UZjQsR9aIg.1730388118286.1	556065882	36880361	1
+-- 63F0564E903242B2A0B02D3EB72D.1728410365885.1	469	53551553	1
+-- _p7YpLub21-cd8YbnckDJWN2XnmV.1729194431120.1	250	46670827	1
+-- _YhuBqXLjqMhOGB8dQ3KgwmEMxnO.1730741363304.2	255	40557450	1
+-- C3B5436F04734D698E701A257B41.1730313226888.1	120	41991232	1
+-- D525975BBD664DD08DEF67C49DB1.1728665975144.1	837	50444114	1
