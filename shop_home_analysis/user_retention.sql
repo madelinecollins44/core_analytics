9bb0daeeeb09fe0e915ccd149ecdfb94d91222cd
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-06 16:59:16 -0500

    Update user_retention.sql

--- shop_home_analysis/user_retention.sql
+++ shop_home_analysis/user_retention.sql
@@ -1,3 +1,15 @@
 --------------------------------------------------------------------------------------------------
 --how often are all users revisiting the shop_home page? 
 --------------------------------------------------------------------------------------------------
+select
+  u.mapped_user_id,
+  e._date,
+  lead(e._date) over (partition by mapped_user_id order by _date) as next_visit_date,
+  date_diff(lead(e._date) over (partition by mapped_user_id order by _date), e._date, day) as days_to_next_visit
+from 
+  etsy-data-warehouse-prod.weblog.events e
+left join 
+  `etsy-data-warehouse-prod.user_mart.user_mapping` u using (user_id)
+where event_type in ('shop_home')
+group by all 
+order by 4 desc
