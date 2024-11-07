6dfb92b43cc5d94997948109767faa1e1c1b38cc
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-07 16:24:10 -0500

    Update listing_views_post_shop_home.sql

--- shop_home_analysis/listing_views_post_shop_home.sql
+++ shop_home_analysis/listing_views_post_shop_home.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------------------------------------
---distribution of recent listing views for shop home visitors (and split by internal SH view vs. landers)
+--distribution of recent listing views for shop home visitors (all traffic)
 ------------------------------------------------------------------------------------------------------
 with first_shop_home_view as (
 select
@@ -82,3 +82,50 @@ where
 group by all 
 )
 select * from etsy-data-warehouse-prod.weblog.events where visit_id in ('Cs2RoVsoE9Df_lgsW4FSG_B7I_-G.1730609731023.1') and sequence_number = 285
+
+------------------------------------------------------------------------------------------------------
+--distribution of recent listing views for shop home visitors (landings)
+------------------------------------------------------------------------------------------------------
+with first_shop_home_view as (
+select
+  visit_id,
+  min(sequence_number) as first_sequence_number
+from 
+  etsy-data-warehouse-prod.weblog.events
+where 
+  event_type in ('shop_home')
+group by all 
+)
+, all_visits as (
+select
+  lv.visit_id,
+  count(listing_id) as total_listing_views,
+  count(case when referring_page_event in ('shop_home') then listing_id end) as shop_home_listing_view,
+  count(case when referring_page_event not in ('shop_home') then listing_id end) as other_listing_view
+from 
+  etsy-data-warehouse-prod.analytics.listing_views lv
+inner join 
+  first_shop_home_view fv using (visit_id)
+inner join 
+  etsy-data-warehouse-prod.weblog.visits v
+    on lv.visit_id=v.visit_id
+where 
+  fv.first_sequence_number < lv.referring_page_event_sequence_number -- listing views happen after shop home 
+  and lv._date >= current_date-30
+  and v._date >= current_date-30
+  and landing_event in ('shop_home')
+group by all 
+)
+select
+  count(distinct visit_id) as visits_with_lv,
+  sum(total_listing_views) as total_lv,
+  sum(shop_home_listing_view) as shop_home_lv,
+  sum(other_listing_view) as other_lv,
+  avg(total_listing_views) as avg_total_lv_per_visit,
+  avg(shop_home_listing_view) as avg_shop_home_lv_per_visit,
+  avg(other_listing_view) as avg_other_lv_per_visit,
+from all_visits
+group by all
+------------------------------------------------------------------------------------------------------
+--distribution of recent listing views for shop home visitors (exclude landings)
+------------------------------------------------------------------------------------------------------
