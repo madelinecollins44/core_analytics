7e7df1338f73076fdfc08f4134c5e997594a7553
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-07 10:10:33 -0500

    Update user_retention.sql

--- shop_home_analysis/user_retention.sql
+++ shop_home_analysis/user_retention.sql
@@ -5,8 +5,8 @@ with users_and_visits as (
 select
   u.mapped_user_id,
   e._date,
-  lead(e._date) over (partition by mapped_user_id order by _date) as next_visit_date,
-  date_diff(lead(e._date) over (partition by mapped_user_id order by _date), e._date, day) as days_to_next_visit
+  lag(e._date) over (partition by mapped_user_id order by _date) as last_visit_date,
+  date_diff(lag(e._date) over (partition by mapped_user_id order by _date), e._date, day) as days_since_last_visit
 from 
   etsy-data-warehouse-prod.weblog.events e
 left join 
@@ -15,11 +15,11 @@ where event_type in ('shop_home')
 group by all 
 )
 select 
-  days_to_next_visit,
+  days_since_last_visit,
   count(distinct mapped_user_id) as users
 from users_and_visits
 group by all 
-order by 1 desc
+order by 1 asc
 
 ---testing
 with users_and_visits as (
