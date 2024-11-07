311ddcceb9313c95a9178d98bd828461d6109568
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-07 12:43:40 -0500

    Update user_retention.sql

--- shop_home_analysis/user_retention.sql
+++ shop_home_analysis/user_retention.sql
@@ -1,5 +1,6 @@
 --------------------------------------------------------------------------------------------------
 --distribution of time since last visit for shop home visitors 
+----users will be double counted if have different days between visits
 --------------------------------------------------------------------------------------------------
 --all shop home traffic 
 with users_and_visits as (
@@ -51,3 +52,25 @@ select * from users_and_visits where days_to_next_visit < 7 limit 5
 -- 2470	2024-10-24	2024-10-18	6
 
 ---testing users with null days between visits 
+with users_and_visits as (
+select
+  u.mapped_user_id,
+  e._date,
+  lag(e._date) over (partition by mapped_user_id order by _date) as last_visit_id,
+  date_diff(e._date, lag(e._date) over (partition by mapped_user_id order by _date), day) as days_since_last_visit
+from 
+  etsy-data-warehouse-prod.weblog.events e
+left join 
+  `etsy-data-warehouse-prod.user_mart.user_mapping` u using (user_id)
+where 
+  event_type in ('shop_home')
+group by all 
+)
+select distinct _date from users_and_visits where days_since_last_visit is null limit 5
+------users with null last_visit_dates
+-- mapped_user_id	_date	last_visit_id	days_since_last_visit
+-- 305	2024-10-18		
+-- 1661	2024-10-21		
+-- 2093	2024-11-02		
+-- 2221	2024-10-08		
+-- 3338	2024-10-21		
