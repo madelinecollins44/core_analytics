411592164b35c761923ee08e057bc759d89107d8
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-08 10:45:51 -0500

    Update shops_per_user.sql

--- shop_home_analysis/shops_per_user.sql
+++ shop_home_analysis/shops_per_user.sql
@@ -22,3 +22,32 @@ select
   avg(unique_transactions) as avg_unique_transactions
 from 
   purchased_from_shops
+
+------------------------------------------------------------------------------------------------------------
+--repeat purchases from the same shop - are there lots of shop ‘loyalists’?
+------------------------------------------------------------------------------------------------------------
+with purchased_from_shops as (
+select
+  tv.user_id, 
+	t.seller_user_id,
+  count(distinct transaction_id) as unique_transactions
+from 
+  etsy-data-warehouse-prod.transaction_mart.transactions_visits tv
+inner join
+	etsy-data-warehouse-prod.transaction_mart.all_transactions t 
+		using (transaction_id)
+where 
+  tv.date >= current_date-365
+group by all 
+)
+select
+  count(distinct case when unique_transactions = 1 then user_id end) as users_purchase_from_shop_1_time,
+  count(distinct case when unique_transactions = 2 then user_id end) as users_purchase_from_shop_2_times,
+  count(distinct case when unique_transactions = 3 then user_id end) as users_purchase_from_shop_3_times,
+  count(distinct case when unique_transactions = 4 then user_id end) as users_purchase_from_shop_4_times,
+  count(distinct case when unique_transactions = 5 then user_id end) as users_purchase_from_shop_5_times,  
+  count(distinct case when unique_transactions = 10 then user_id end) as users_purchase_from_shop_10_times,
+  count(distinct case when unique_transactions = 15 then user_id end) as users_purchase_from_shop_15_times,
+  count(distinct case when unique_transactions = 20 then user_id end) as users_purchase_from_shop_20_times,  
+  count(distinct case when unique_transactions = 30 then user_id end) as users_purchase_from_shop_30_times
+from purchased_from_shops
