9fe5dc95f02954a11d544a372b32a64b2a8fa74f
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-11-14 13:52:11 -0500

    Update onboarding_funnel_eventing.sql

--- boe_onboarding/onboarding_funnel_eventing.sql
+++ boe_onboarding/onboarding_funnel_eventing.sql
@@ -1,3 +1,34 @@
+------------------------------------------------
+--find first BOE visit for each browser
+------------------------------------------------
+CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.madelinecollins.boe_first_visits` AS (
+select
+  browser_id
+  , user_id
+  , platform
+  , event_source
+  , visit_id
+  , registered
+  , new_visitor
+  , landing_event
+  , exit_event
+  , converted
+  , case when user_id is not null then 1 else 0 end as signed_in
+  , _date 
+  , start_datetime
+  , row_number() over (partition by browser_id order by start_datetime asc) as visit_rnk
+  , lead(start_datetime, 1) over (partition by browser_id order by start_datetime asc) as next_visit
+  , lead(start_datetime, 2) over (partition by browser_id order by start_datetime asc) as third_visit
+from `etsy-data-warehouse-prod.weblog.visits`
+where
+    extract(year from _date) >= 2017
+    and platform = 'boe'
+    and pages_seen > 1 -- filters out visits that didnt see a primary page, ensures more purposeful visits  
+);
+
+------------------------------------------------
+--pull all events associated with onboarding
+------------------------------------------------
  create or replace table etsy-data-warehouse-dev.madelinecollins.app_onboarding_events as (
  select
     beacon.event_name  as event_name,
@@ -9,16 +40,16 @@
   where date(_partitiontime) >= current_date-30
       and (beacon.event_name in (
       ---- LOG IN SPLASH SCREEN
-      'sign_in_screen', -- this also captures sign in from homescreen. distinction made by fullgate property, fullgate = true 
+      'sign_in_screen', -- PRIMARY EVENT FOR THIS SCREEN. distinction made by fullgate property, fullgate = true 
       'continue_as_guest_tapped', -- continue as guest 
-      'BOE_social_sign_in_tapped',-- log in w socials 
-      'BOE_etsy_sign_in_tapped', -- log in w email 
+      'BOE_social_sign_in_tapped',-- log in w socials BUT fires across screens 
+      'BOE_etsy_sign_in_tapped', -- log in w email  BUT fires across screens 
       ----REGISTRAION WEB VIEW
       'join_submit',
       'BOE_email_sign_in_webview_cancelled',
       'register_view'
       ----SIGN IN WEB VIEW
-      'signin_submit',
+      'signin_submit', -- PRIMARY EVENT FOR THIS SCREEN.
       'magic_link_click',
       'magic_link_send',
       'magic_link_redeemed',
@@ -31,43 +62,44 @@
       'keep_me_signed_in_unchecked',
       'login',
       'BOE_email_sign_in_webview_cancelled',
-      'login_view'
+      'login_view' -- would be great to use this but unsure if this fires in beacons table
       ----NOTIFICATIONS OPT IN 1 
       'notification_registration_interstitial_enable_tapped',
       'notification_registration_interstitial_dismiss_tapped',
       'update_setting',
-      'notification_registration_interstitial_viewed',
+      'notification_registration_interstitial_viewed', -- PRIMARY EVENT FOR THIS SCREEN.
       ----NOTIFICATIONS OPT IN 2
-      'push_prompt_permission_granted',
-      'push_prompt_permission_denied',
+      'push_prompt_permission_granted', -- PRIMARY EVENT FOR THIS SCREEN.
+      'push_prompt_permission_denied', -- PRIMARY EVENT FOR THIS SCREEN.
       ----APP TRACKING TRANSPARENCY PROMPT
-      'app_tracking_transparency_system_prompt_denied_tapped',
-      'app_tracking_transparency_system_prompt_authorized_tapped',
+      'app_tracking_transparency_system_prompt_denied_tapped', -- PRIMARY EVENT FOR THIS SCREEN.
+      'app_tracking_transparency_system_prompt_authorized_tapped', -- PRIMARY EVENT FOR THIS SCREEN.
       ----SIGN IN FROM HOMESCREEN
       'login_view',
       'BOE_social_sign_in_tapped',
+      'sign_in_screen', -- PRIMARY EVENT FOR THIS SCREEN. distinction made by fullgate property, fullgate = false 
       ----FAVORITES QUIZ 
-      'onboarding_favorites-q4-2019_viewed',
-      'onboarding_faves_tapped_skip',
+      'favorites_onboarding_skip_button_tapped',
       'favorites_onboarding_done_button_tapped',
-      'scrolled_past_onboarding_favorites-q4-2019',
-      ----INITIAL CONTENT (SKIPPED QUIZ)
-      -- 'homescreen',
-      'homescreen_recommended_Categories_seen',
-      'boe_homescreen_popular_-clusters_seen',
-      'scrolled_past_boe_homescreen_recs_placholder_-module_4',
-      'recommendations_module_seen'
-      ----INITIAL CONTENT (COMPLETED QUIZ)
-      'homescreen_top_banner_marketing_Merch3_event_viewed',
-      'boe_homescreen_home_hub_viewed',
-      'scrolled_past_boe_homescreen_home_hub',
-      'scrolled_past_homescreen_our_picks',
-      'recommended'
-      ----HOMESCREEN WITH ACTIVITY 
-      'boe_evergreen_interests_recs_module_delivered',
-      'boe_homescreen_home_hub_delivered',
-      'boe_active_mission_recs_module_delivered',
-      'boe_homescreen_tab_delivered')
+      'favorites_onboarding_viewed')-- PRIMARY EVENT FOR THIS SCREEN.
+      ----------the following sections are removed due to the fact that these are not onboarding requirements. 
+      -- ----INITIAL CONTENT (SKIPPED QUIZ)
+      -- -- 'homescreen',
+      -- 'homescreen_recommended_Categories_seen',
+      -- 'boe_homescreen_popular_-clusters_seen',
+      -- 'scrolled_past_boe_homescreen_recs_placholder_-module_4',
+      -- 'recommendations_module_seen'
+      -- ----INITIAL CONTENT (COMPLETED QUIZ)
+      -- 'homescreen_top_banner_marketing_Merch3_event_viewed',
+      -- 'boe_homescreen_home_hub_viewed',
+      -- 'scrolled_past_boe_homescreen_home_hub',
+      -- 'scrolled_past_homescreen_our_picks',
+      -- 'recommended'
+      -- ----HOMESCREEN WITH ACTIVITY 
+      -- 'boe_evergreen_interests_recs_module_delivered',
+      -- 'boe_homescreen_home_hub_delivered',
+      -- 'boe_active_mission_recs_module_delivered',
+      -- 'boe_homescreen_tab_delivered')
     or       
   ----SIGNED OUT HOMESCREEN
       (beacon.event_name = "homescreen_complementary"
